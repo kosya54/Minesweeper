d44c23906c106ad52e973853a4cf9e6dbaa67c2f
kosya54 <49726304+kosya54@users.noreply.github.com>
2020-03-13 14:46:37 +0300

    Update GameSessionController.java

--- src/com/kosenko/minesweeper/controllers/GameSessionController.java
+++ src/com/kosenko/minesweeper/controllers/GameSessionController.java
@@ -1,12 +1,26 @@
 package com.kosenko.minesweeper.controllers;
 
 import com.kosenko.minesweeper.models.Minefield;
+import com.kosenko.minesweeper.gui.Cell;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.IOException;
+
+import com.google.gson.JsonObject;
 
 public class GameSessionController {
     private final static int DEFAULT_GRID_LENGTH = 9;
     private final static int DEFAULT_COUNT_MINES = 10;
     private final static int MAX_GRID_LENGTH = 25;
     private final static int MAX_COUNT_MINES = 300;
+    
+//    private final static String SAVES_PATH = "src/com/kosenko/minesweeper/saves/";
+    private final static String SAVES_PATH = "D:/Java/Minesweeper/src/com/kosenko/minesweeper/saves/";
+    private final static String FILE_EXTENSION = ".sav";
 
     public static int getDefaultGridLength() {
         return DEFAULT_GRID_LENGTH;
@@ -63,5 +77,36 @@ public class GameSessionController {
 
         return minefield.generateMinefield();
     }
-}
 
+    private static int getCountOpenedCells(Cell[][] cells) {
+        int count = 0;
+        for (Cell[] innerArray : cells) {
+            for (Cell cell : innerArray) {
+                if (cell.isOpened()) {
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static boolean isWon(JsonObject gameSessionParameters, Cell[][] cells) {
+        int totalCells = gameSessionParameters.get("columns").getAsInt() * gameSessionParameters.get("rows").getAsInt();
+        int countOpened = GameSessionController.getCountOpenedCells(cells);
+        int mines = gameSessionParameters.get("mines").getAsInt();
+
+        return totalCells - countOpened == mines;
+    }
+
+    public static boolean isLose(int value) {
+        return value == getMineValue();
+    }
+
+    public static void writeHighscore(JsonObject gameSession) throws IOException {
+        File file = new File(SAVES_PATH, "Highscore" + FILE_EXTENSION);
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), StandardCharsets.UTF_8))) {
+            writer.write(gameSession.toString());
+        }
+    }
+}
